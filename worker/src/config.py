c746dd1451976f9bde9c0b0e823cefe7e5f72069
Kaka0dz7 <kauachamerobr@gmail.com>
2026-04-27 19:49:17 +0000

    conected api 2

--- worker/src/config.py
+++ worker/src/config.py
@@ -1,19 +1,16 @@
+import os
+from dotenv import load_dotenv
 import moviepy.config as cfg
+
+load_dotenv()
 
 cfg.change_settings({
     "IMAGEMAGICK_BINARY": r"C:\Program Files\ImageMagick-7.1.2-Q16-HDRI\magick.exe"
 })
 
-import os
-from dotenv import load_dotenv
-import moviepy.config as cfg
 
-# Carrega variáveis de ambiente de um arquivo .env (crie um na raiz do projeto)
-load_dotenv()
 
-cfg.change_settings({
-    "IMAGEMAGICK_BINARY": r"C:\Program Files\ImageMagick-7.x.x\magick.exe"
-})
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-# Configuração da IA
-GEMINI_API_KEY = os.getenv("AIzaSyCKZvR2k_aLY3DTPuX-qtSswB0hChi6tEE")
+if not GEMINI_API_KEY:
+    raise ValueError("⚠️ GEMINI_API_KEY não encontrada. Verifique seu arquivo .env")
--- worker/src/main.py
+++ worker/src/main.py
@@ -1,5 +1,5 @@
-import config
-from core.video_processor import process_video
+from src.config import GEMINI_API_KEY
+from src.core.video_processor import process_video
 
 try:
     process_video("assets/input.mp4", "output/output.mp4")
